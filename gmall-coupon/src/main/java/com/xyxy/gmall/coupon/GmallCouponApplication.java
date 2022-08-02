fa8c5dacb3f21acc838b611ba232ac36694639d2
jackyzou <jackyzou2510@gmail.com>
2022-08-02 15:50:12 +0800

    项目初始结构创建

---
+++ gmall-coupon/src/main/java/com/xyxy/gmall/coupon/GmallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.xyxy.gmall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallCouponApplication.class, args);
+    }
+
+}
---
+++ gmall-member/src/test/java/com/xyxy/gmall/member/GmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyxy.gmall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
